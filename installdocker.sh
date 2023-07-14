014248d9db71744fc6d1136573c4608448f82be2
jinjinlei2108 <jinjinlei2108@hotmail.com>
2023-07-14 15:06:02 +1000

    update installDocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+echo "starting to install docker engine"
+
+sudo apt-get install -y ca-certificates curl gnupg
 
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -12,7 +13,7 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+sudo apt-get -y update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
