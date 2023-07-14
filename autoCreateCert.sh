03a5c84cf4e036f7ececffc8a3e5beef91c1dfe9
jinjinlei2108 <jinjinlei2108@hotmail.com>
2023-07-14 10:59:19 +1000

    update autoCreateCert

--- autoCreateCert.sh
+++ autoCreateCert.sh
@@ -16,6 +16,34 @@ fi
 echo 'register certificate'
 docker exec -it "$container" certbot --nginx --agree-tos --email "$email" -d "$domain"
 
+sudo chmod +r /etc/letsencrypt/live
+
+
+config_block=$(cat <<EOF
+server {
+    listen 80;
+    server_name "$domain";
+    return 301 https://\$host\$request_uri;
+}
+
+server {
+    listen 443 ssl;
+    server_name $domain;
+
+    location / {
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$remote_addr;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Host \$host;
+        proxy_pass http://odoo:8069;
+        add_header Content-Security-Policy upgrade-insecure-requests;
+    }
+}
+
+EOF
+)
+
+
 echo "server {
     listen 80;
     server_name "$domain";
@@ -39,9 +67,19 @@ server {
     }
 }" | sudo tee -a /home/ubuntu/get2knowOdoo/config/nginx/default.conf
 
+if [ -f "/etc/letsencrypt/live/$domain/fullchain.pem" ] && [ -f "/etc/letsencrypt/live/$domain/privkey.pem" ]; then
+    
+    echo "SSL certificate exists. Updating Nginx configuration."
+    
+    echo "$config_block" | sudo tee -a /home/ubuntu/get2knowOdoo/config/nginx/default.conf
 
-echo 'reload nginx'
+    echo 'reload nginx'
 
-docker exec "$container" nginx -s reload
+    docker exec "$container" nginx -s reload
+
+else
+    echo "SSL certificate does not exist. Skipping Nginx configuration."
+fi
+
 
 
