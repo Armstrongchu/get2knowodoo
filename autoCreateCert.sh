16bed6a57a9f74b564e275a9b290aaf90dd9741b
jinjinlei2108 <jinjinlei2108@hotmail.com>
2023-07-14 11:56:15 +1000

    update autoCreateCert

--- autoCreateCert.sh
+++ autoCreateCert.sh
@@ -16,7 +16,7 @@ fi
 echo 'register certificate'
 docker exec -it "$container" certbot --nginx --agree-tos --email "$email" -d "$domain"
 
-sudo chmod +r /etc/letsencrypt/live
+sudo chmod +r /home/ubuntu/get2knowOdoo/config/certbot/live
 
 
 config_block=$(cat <<EOF
@@ -43,29 +43,6 @@ server {
 EOF
 )
 
-
-echo "server {
-    listen 80;
-    server_name "$domain";
-    return 301 https://\$host\$request_uri;
-}
-
-server {
-    listen 443 ssl;
-    server_name $domain;
-
-    ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
-
-    location / {
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$remote_addr;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header Host \$host;
-        proxy_pass http://odoo:8069;
-        add_header Content-Security-Policy upgrade-insecure-requests;
-    }
-}" | sudo tee -a /home/ubuntu/get2knowOdoo/config/nginx/default.conf
 
 if [ -f "/etc/letsencrypt/live/$domain/fullchain.pem" ] && [ -f "/etc/letsencrypt/live/$domain/privkey.pem" ]; then
     
